c790ad1d95421a6fd7cef16b090646c537eb0578
Towards The Sun <37067056+Xiaomimimiya@users.noreply.github.com>
2021-09-25 22:39:50 +0800

    Delete 02-basic-webpack-css directory

--- 02-basic-webpack-css/src/js/element.js
+++
@@ -1,9 +0,0 @@
-// import "css-loader!../css/style.css"
-// 内联方式配置Css-loader
-import "../css/style.css";
-import "../css/title.less";
-
-const divEl = document.createElement("div");
-divEl.className = "title";
-divEl.innerHTML = "hello world";
-document.body.appendChild(divEl);
--- 02-basic-webpack-css/src/js/test.js
+++
@@ -1,6 +0,0 @@
-import "../css/style.css";
-
-const btn = document.createElement("div");
-btn.className = "click";
-btn.innerHTML = "test_btn";
-document.body.appendChild(btn);
