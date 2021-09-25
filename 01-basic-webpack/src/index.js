ab7f5e1fcfec84f9ebee9f0030bc8f4c1778bf9b
Towards The Sun <37067056+Xiaomimimiya@users.noreply.github.com>
2021-09-25 22:31:32 +0800

    Add files via upload

---
+++ 01-basic-webpack/src/index.js
@@ -0,0 +1,7 @@
+// 导入math
+import { sum } from "./js/math";
+// 导入formate
+const { priceFormat } = require("./js/format");
+
+console.log(sum(20, 300));
+console.log(priceFormat());
---
+++ 01-basic-webpack/src/js/math.js
@@ -0,0 +1,4 @@
+// Es module的导出
+export function sum(num1, num2) {
+    return num1 + num2;
+}
