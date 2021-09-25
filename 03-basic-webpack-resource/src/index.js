d83ebd4f5b04fd87fdea820729c2b70c2da759c5
Towards The Sun <37067056+Xiaomimimiya@users.noreply.github.com>
2021-09-25 22:33:08 +0800

    Add files via upload

---
+++ 03-basic-webpack-resource/src/index.js
@@ -0,0 +1,9 @@
+// 导入math
+import { sum } from "./js/math";
+// 导入formate
+const { priceFormat } = require("./js/format");
+import "./js/element";
+
+
+console.log(sum(20, 300));
+console.log(priceFormat());
