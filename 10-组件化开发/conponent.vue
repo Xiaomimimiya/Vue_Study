ab77cbc54b03f36891d644ce66d40bc85bfe73eb
Towards The Sun <37067056+Xiaomimimiya@users.noreply.github.com>
2021-09-25 22:27:44 +0800

    Add files via upload

---
+++ 10-组件化开发/conponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <input type="text" v-model="message">
+        <h2>{{message}}</h2>
+        <h2>{{title}}</h2>
+        <p>{{desc}}</p>
+        <button @click="btnclick">按钮</button>
+    </div>
+
+</template>
+<script>
+
+export default({
+    data() {
+        return {
+            message: "hello world",
+            title: "haaaa",
+            desc: "xixixi"
+            }
+        },
+    methods: {
+        btnclick() {
+            console.log("按钮发生点击");
+            }
+        }
+
+});
+</script>
+
+<style scoped>
+
+</style>
